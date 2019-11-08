294ab3aaf04ddaa4b810aaa5206102762ce33dfa
tom hesen <tom@hesen-helmond.nl>
2019-11-08 12:59:41 +0100

    demo frontend

---
+++ demo-frontend/demofrontend.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="inc/css/bootstrap.min.css">
+
+ <!-- custom CSS -->
+<link rel="stylesheet" type="text/css" href="inc/css/demofrontend.css">
+    <title>Demo-Frontend</title>
+</head>
+
+<body>
+    <p class="container welcomeP">Welcome</p>
+    <button class="frontendButton" href="loginPage.php">click here</button>    
+</body>
+
+ 
