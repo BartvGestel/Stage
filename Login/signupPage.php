ba852a5969654a0ea0df2265db8a636fde615b7b
maik rutten <maik.rutten@hotmail.com>
2019-11-20 13:30:55 +0100

    signUp page styling

--- Login/signupPage.php
+++ Login/signupPage.php
@@ -1,5 +1,16 @@
-
-<?php include("inc/header.php") ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font Awsome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+     <!-- Custom CSS -->
+     <link rel="stylesheet" type="text/css" href="css/style.css">
 <body>
     <form action="inc/signup.php" method="POST" id="container">
     <div id="error">
@@ -23,13 +34,40 @@
                 }
                 ?>
         </div>
-        *Username: <br> <input name="uid" align="right" class="input-box" type="text"> <br>
-        *First name: <br> <input name="first" align="right" class="input-box" type="text"> </br>
-        *Second name: <br> <input name="last" align="right" class="input-box" type="text"> </br>
-        *Password: <br> <input name="pwd" align="right" class="input-box" type="password"> </br>
-        <input type="submit" name="submit" value="Register">
-        <p>All items with a star are mandatory to fill in</p>
+        <div  class="container Register-Container">
+        <H2 class="Login-Title"><i class="fas fa-sign-in-alt"></i> Register</H2>
+        <form class="Login-Form" action="Inc/RegisterLoad.php" method="POST">
+            <div class="form-group">
+                <label class="Label">First Name</label>
+                <input type="text" class="form-control" name="first"  placeholder="Enter First Name">
+            </div>
+            <div class="form-group">
+                <label class="Label">Last Name</label>
+                <input type="text" class="form-control" name="last"  placeholder="Enter Last Name">
+            </div>
+            <div class="form-group">
+                <label class="Label">Username</label>
+                <input type="text" class="form-control" name="uid"  placeholder="Enter Username">
+            </div>
+            <div class="form-group">
+                <label class="Label">Password</label>
+                <input type="password" class="form-control" name="pwd"  placeholder="Enter Password">
+            </div>
+            <!-- submit button submits data to registerLoad.php, Login button goes to Login.php -->
+            <div class="row justify-content-around">
+                <a class="btn btn-light Register-Button" href="loginPage.php"><i class="fas fa-sign-in-alt"></i>Login</a>
+                <button type="submit" class="btn btn-light Register-Button" name="submit" value="Register"><i class="far fa-user"></i> Submit</button>
+            </div>
+            
+        </form>
+    </div>
+
     </form>
 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="JS/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="JS/main.js"></script>
 </body>
 </html>
