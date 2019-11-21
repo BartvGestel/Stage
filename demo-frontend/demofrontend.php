24aa65d2eb033f2efe07595fbce5e266c844bd31
maik rutten <maik.rutten@hotmail.com>
2019-11-21 18:27:58 +0100

    styling change again

--- demo-frontend/demofrontend.php
+++ demo-frontend/demofrontend.php
@@ -5,7 +5,12 @@
         <meta charset="utf-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font Awsome CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+     
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="inc/css/bootstrap.min.css">
 
@@ -16,11 +21,21 @@
 </head>
 <body>
     <div class="container welcomeD">
-        <p class="container justify-content-center welcomeP">Welcome</p>
+        <p class="container justify-content-center welcomeP"><i class="fas fa-globe"></i> Welcome <i class="fas fa-globe"></i></p>
         <div class="easy"><img src="../stage-code/images/Easypeasycoding.png"></div>
-        <div class="Click"><a class="container welcomeA" href="../Login/loginPage.php">Click Here To Login</a></div>
+        <div class="Click"><a class="container welcomeA" href="../Login/loginPage.php"><i class="fas fa-hand-point-right"></i> Click Here To Login <i class="fas fa-hand-point-left"></i></a></div>
         <div class="quote"><h2>Learn as much as you can while you are young, since life becomes too busy later!</h2></div>
     </div>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script>"../keyboard/inc/js/jkeyboard.js"</script>
+<!-- loading the boodstrap -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="JS/main.js"></script>
+  <script src="https://code.jquery.com/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
  
