9f84eea8fd9868d14b23d8c1d8a9e7119e513db3
maik rutten <maik.rutten@hotmail.com>
2019-11-28 10:36:33 +0100

    changed the onscreen keyboard so it will be possible to use on a phone

--- Stage-code/Inc/getSound.php
+++ Stage-code/Inc/getSound.php
@@ -79,23 +79,23 @@ var score2 = 0;
             echo "<img id='img1' src='images/starRight.png'>";
             echo " Correct"; 
             echo "</div>";
-            ?>
-                <script>
-                document.getElementById("score1").innerHTML = score1;
+            echo "
+                <script type=\"text/javascript\">
+                document.getElementById('score1').innerHTML = score1;
                 score1++;
                 </script>
-            <?php
+                ";
         }else{
             echo "<div class=rightwrong>";
             echo "<img id='img2' src='images/starWrong.png'>";
             echo "incorrect";
             echo "</div>";
-            ?>
-                <script>
-                document.getElementById("score2").innerHTML = score2;
+            echo "
+                <script type=\"text/javascript\">
+                document.getElementById('score2's).innerHTML = score2;
                 score2++;
                 </script>
-            <?php
+                ";
         }
     }
 ?>
